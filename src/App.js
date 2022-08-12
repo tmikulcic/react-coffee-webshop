a55394678c4c37b94bdb6ca9d1095c7a34cf58e1
gtx <tom.mikulcic@gmail.com>
2022-08-12 08:44:12 +0200

    Add functional routing to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home/Home';
+import Menu from './components/Menu/Menu';
+import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className='main-container'>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/menu' element={<Menu />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 export default function Navbar() {
   return (
     <div className='navbar'>
-      <div className='navbar-name'>Tom's Coffee Brewery</div>
+      <Link to='/' className='navbar-name'>
+        Tom's Coffee Brewery
+      </Link>
       <div className='navbar-links'>
-        <div className='link'>Home</div>
-        <div className='link'>Menu</div>
-        <div className='link'>About</div>
-        <div className='link'>Contact</div>
+        <Link to='/' className='link'>
+          Home
+        </Link>
+        <Link to='/menu' className='link'>
+          Menu
+        </Link>
+        <Link to='/about' className='link'>
+          About
+        </Link>
+        <Link to='/contact' className='link'>
+          Contact
+        </Link>
       </div>
     </div>
   );
