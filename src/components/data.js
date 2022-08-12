8f6381aa5883ef9cc1d105b936c7c21bb6b57eb1
Tomislav <tom.mikulcic@gmail.com>
2022-08-12 20:51:56 +0200

    Add more items to data

--- src/components/data.js
+++ src/components/data.js
@@ -8,8 +8,38 @@ export default [
 
   {
     id: 2,
-    name: 'Coffee with milk',
+    name: 'Coffee with Milk',
     img: './images/coffee.png',
+    price: 1.5,
+  },
+  {
+    id: 3,
+    name: 'Ice Coffee',
+    img: './images/ice-coffee.png',
+    price: 2,
+  },
+  {
+    id: 4,
+    name: 'Coffee To-Go',
+    img: './images/coffee-togo.png',
+    price: 1.5,
+  },
+  {
+    id: 5,
+    name: `Tom's Premium Brewed Coffee (250g)`,
+    img: './images/coffee-bag.png',
+    price: 1.5,
+  },
+  {
+    id: 6,
+    name: `Tom's Premium Brewed Coffee (500g)`,
+    img: './images/coffee-bag.png',
+    price: 1.5,
+  },
+  {
+    id: 7,
+    name: `Tom's Premium Brewed Coffee (1kg)`,
+    img: './images/coffee-bag.png',
     price: 1.5,
   },
 
